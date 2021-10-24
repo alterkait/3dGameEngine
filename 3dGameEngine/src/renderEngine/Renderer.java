4c7ef4d483e84d0317be412b435439743f3390cc
Abdulrahman AlTerkai <Abdulrahman AlTerkai@DESKTOP-PEAPL9I>
2021-10-24 19:41:31 +0100

    created projection matrix method

--- 3dGameEngine/src/renderEngine/Renderer.java
+++ 3dGameEngine/src/renderEngine/Renderer.java
@@ -1,5 +1,6 @@
 package renderEngine;
 
+import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
@@ -43,6 +44,22 @@ public class Renderer {
 	}
 	
 	private void createProjectionMatrix() {
+		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+		float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV/2f))) * aspectRatio);
+		float x_scale = y_scale / aspectRatio;
+		float frustum_length = FAR_PLANE - NEAR_PLANE;
+		
+		projectionMatrix = new Matrix4f();
+		
+		projectionMatrix.m00 = x_scale;
+		projectionMatrix.m11 = y_scale;
+		
+		projectionMatrix.m22 = -((FAR_PLANE + NEAR_PLANE)/ frustum_length);
+		projectionMatrix.m23 = -1;
+		
+		projectionMatrix.m32 = -((2* NEAR_PLANE * FAR_PLANE) / frustum_length);
+		projectionMatrix.m33 = 0;
+		
 		
 	}
 
