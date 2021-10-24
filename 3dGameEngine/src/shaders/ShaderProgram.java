b9a1b3fa068496e0a7930343dbae5609cd096db4
Abdulrahman AlTerkai <Abdulrahman AlTerkai@DESKTOP-PEAPL9I>
2021-10-24 18:44:35 +0100

    Fixed binding attributes

--- 3dGameEngine/src/shaders/ShaderProgram.java
+++ 3dGameEngine/src/shaders/ShaderProgram.java
@@ -16,13 +16,14 @@ public abstract class ShaderProgram {
 	public ShaderProgram(String vertexFile, String fragmentFile) {
 		vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
 		fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
-		
 		programID = GL20.glCreateProgram();
+		
 		GL20.glAttachShader(programID, vertexShaderID);
 		GL20.glAttachShader(programID, fragmentShaderID);
 		
-		GL20.glLinkProgram(programID);
+		bindAttributes();
 		
+		GL20.glLinkProgram(programID);
 		GL20.glValidateProgram(programID);
 	}
 	
