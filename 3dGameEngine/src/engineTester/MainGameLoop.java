9c05db76e61d20b0ddabc0584a79b56007e85f4c
Abdulrahman AlTerkai <Abdulrahman AlTerkai@DESKTOP-PEAPL9I>
2021-12-15 19:38:20 +0000

    Render Square

--- 3dGameEngine/src/engineTester/MainGameLoop.java
+++ 3dGameEngine/src/engineTester/MainGameLoop.java
@@ -3,18 +3,39 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		//THE DATA
+		float[] vertices = {
+				-0.5f, -0.5f, 0.0f,	//left bottom
+				0.5f, -0.5f, 0.0f,	//right bottom
+				-0.5f, 0.5f, 0.0f,	//left top
+				
+				-0.5f, 0.5f, 0.0f,	//left top
+				0.5f, -0.5f, 0.0f,	//right bottom
+				0.5f, 0.5f, 0.0f	//top right
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
-			
+			renderer.prepare();
+			renderer.render(model);
 			
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
 	}
--- 3dGameEngine/src/renderEngine/DisplayManager.java
+++ 3dGameEngine/src/renderEngine/DisplayManager.java
@@ -24,6 +24,7 @@ public class DisplayManager {
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
 			Display.create(new PixelFormat(), attribs);
 			Display.setTitle("AlTerkait's Game Engine");
+			System.out.println("Working fine");
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
