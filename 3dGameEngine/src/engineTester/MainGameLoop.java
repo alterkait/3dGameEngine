8c93c6a24d59e7eff82c776ba82b481b2f52dad0
Abdulrahman AlTerkai <Abdulrahman AlTerkai@DESKTOP-PEAPL9I>
2021-12-16 02:16:29 +0000

    3D Cube

--- 3dGameEngine/src/engineTester/MainGameLoop.java
+++ 3dGameEngine/src/engineTester/MainGameLoop.java
@@ -3,6 +3,7 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
@@ -23,38 +24,96 @@ public class MainGameLoop {
 		
 		//THE DATA
 		float[] vertices = {
-				-0.5f, 0.5f, 0.0f,	//v0
-				-0.5f, -0.5f, 0.0f,	//v1
-				0.5f, -0.5f, 0.0f,	//v2
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
 				
-				0.5f, 0.5f, 0.0f	//v3
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
 		};
 		
 		int[] indices = {
-				0,1,3,		//v0 v1 v3		top left triangle
-				3,1,2		//v3 v1 v2		bottom right triangle
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
 		};
 		
 		float[] textureCoords = {
-				0,0,	//v0
-				0,1,	//v1
-				1,1,	//v2
-				1,0		//v3
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("test_image"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("IMAGE"));
 		TexturedModel staticModel = new TexturedModel(model, texture);
 		Entity entity = new Entity(staticModel, new Vector3f(0,0,-1),0,0,0,1);
+		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, -0.2f);
-			//entity.increaseRotation(0, 0, 0);
-			//System.out.println("X: " + entity.getRotY());
+			entity.increasePosition(0, 0, 0);
+			camera.move();
+			entity.increaseRotation(1, 1, 0);
+			//System.out.println("Z: " + entity.getPosition());
+			System.out.println("Camera Pos : " + camera.getPosition());
 			
 			renderer.prepare();
 			
 			shader.start();
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			
