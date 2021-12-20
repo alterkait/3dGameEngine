e7e3656abffd4aa5ef252e896925c94d1463835f
Abdulrahman AlTerkai <Abdulrahman AlTerkai@DESKTOP-PEAPL9I>
2021-12-20 03:52:33 +0000

    Optimising

--- 3dGameEngine/src/engineTester/MainGameLoop.java
+++ 3dGameEngine/src/engineTester/MainGameLoop.java
@@ -10,9 +10,8 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
 import textures.ModelTexture;
 
 public class MainGameLoop {
@@ -21,8 +20,6 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
 		
 		//THE DATA
 		RawModel model = OBJLoader.loadOBJModel("dragon", loader);
@@ -37,25 +34,20 @@ public class MainGameLoop {
 		
 		Camera camera = new Camera();
 		
+		MasterRenderer renderer = new MasterRenderer();
 		while(!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, 0);
+			
 			camera.move();
-			entity.increaseRotation(0, 1, 0);
-			//System.out.println("Z: " + entity.getPosition());
+			
+			renderer.render(light, camera);
 			System.out.println("Camera Pos : " + camera.getPosition());
 			
-			renderer.prepare();
 			
-			shader.start();
-			shader.loadLight(light);
-			shader.loadViewMatrix(camera);
-			renderer.render(entity, shader);
-			shader.stop();
 			
 			DisplayManager.updateDisplay();
 		}
 		
-		shader.cleanUp();
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 		
